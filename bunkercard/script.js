98c268d3d687c48e9f1992e1a0e9ae503f760a7f
Fyoqusi <97985893+Fyoqusi@users.noreply.github.com>
2024-12-26 11:14:34 +0100

    Added title screen

---
+++ bunkercard/script.js
@@ -0,0 +1,271 @@
+// Фіктивний список характеристик
+const professions = [
+  "Лікар-хірург",
+  "Інженер-механік",
+  "Вчитель фізкультури",
+  "Архітектор",
+  "Кухар",
+  "Пілот",
+  "Художник",
+  "Програміст",
+  "Медсестра",
+  "Фермер",
+  "Психолог",
+  "Снайпер",
+  "Пожежник",
+  "Астронавт",
+  "Дизайнер",
+  "Юрист",
+  "Музикант",
+  "Фотограф",
+  "Офіціант",
+  "Будівельник",
+  "Клоун",
+  "Актор театру",
+  "Інстаграм-блогер",
+  "Столяр",
+  "Безробітний",
+  "Еколог",
+  "Лікар-вірусолог",
+  "Дипломат",
+  "Поліцейський",
+  "Працівник швидкої",
+  "Електрик",
+  "Лікар-терапевт",
+  "Дроворуб",
+  "Викидайло",
+  "Священник",
+];
+const healths = [
+  "Ідеальне здоров'я",
+  "Пневмонія",
+  "Алкоголізм",
+  "ВІЛ",
+  "Вітрянка",
+  "Амнезія",
+  "Грип",
+  "Застуда",
+  "Мігрень",
+  "Алергія на пил",
+  "Немає лівої ноги",
+  "Немає двох пальців на лівій руці",
+  "Депресія",
+  "Синдром Туретта",
+  "Ідеальне здоров'я",
+  "Астма",
+  "Цукровий діабет 1-го типу",
+  "Гіпертонія",
+  "Гастрит",
+  "Карлик",
+  "Грибок нігтів",
+  "Ожиріння",
+  "Наркоманія",
+  "Лудоманія",
+  "Шизофренія",
+  "Біполярний розлад",
+  "Повна сліпота",
+];
+const genders = ["Чоловік", "Жінка"];
+const hobbies = [
+  "Подорожі",
+  "Футбол",
+  "Готування їжі",
+  "Гірські походи",
+  "Велоспорт",
+  "Читання книг",
+  "Танці",
+  "Риболовля",
+  "Виноробство",
+  "Волонтерство",
+  "Пошиття",
+  "Плавання",
+  "Астрономія",
+  "Сталкінг",
+  "Важка атлетика",
+  "Легка атлетика",
+  "Фотографування",
+  "Бокс",
+  "Ботаніка",
+  "Садівництво",
+  "В'язання",
+  "Гончарство",
+  "Малювання",
+  "Програмування",
+  "Хобіхорсинг",
+];
+const facts = [
+  "Пограбував власного дідуся",
+  "Був у комі 14 років",
+  "Знає 5 мов",
+  "Залишався на другий рік в школі",
+  "Вегетаріанець",
+  "Має дві вищі освіти",
+  "Грає у водне поло",
+  "Палить цигарки",
+  "Має чотирьох дітей",
+  "Вірить у всесвітню змову",
+  "Проходив курс воєнної підготовки",
+  "Отримав у спадок 1 мільйон доларів",
+  "Практикується у віщуванні",
+  "Поцупив сирок з АТБ",
+  "Знається на народній медицині",
+  "Колабораціоніст",
+  "Знайомий з президентом",
+  "Не носить спідню білизну",
+  "Пройшов курс домедичної допомоги",
+  "Частково винен у катастрофі",
+  "Перепливав Тису",
+  "Був у шлюбі тричі",
+];
+const baggagelist = [
+  "Енциклопедія юного натураліста",
+  "Чіхуахуа",
+  "Компас",
+  "Ножиці",
+  "Карти",
+  "Молоток",
+  "Викрутка",
+  "Мультиінструмент",
+  "Насінинка пшениці",
+  "Телефон",
+  "Робот-пилосос",
+  "Шахи",
+  "Збірник 4500 анекдотів",
+  "Безлімітна картка",
+  "Турецький килим",
+  "Туалетний папір",
+  "Набір для в'язання",
+  "Парасолька",
+  "Ящик полуниці",
+  "Лампочка",
+  "Біблія",
+  "Фікус",
+  "Коробка патронів 5.56 мм",
+  "Цегла",
+  "Отрута для мишей",
+];
+const actioncards = [
+  "У цьому раунді ти не голосуєш, але голоси проти вибраного учасника подвоюються",
+  "Цього раунду проти тебе не можуть голосувати",
+  "Ти можеш обмінятися багажем з однією людиною",
+  "Ти можеш обмінятися здров'ям з однією людиною",
+  "Ти можеш обмінятися професією з однією людиною",
+  "Ти можеш обмінятися хобі з однією людиною",
+  "Ти можеш обмінятися фактом з однією людиною",
+  "Омолоди вибраного гравця на 15 років, якщо йому 33 і більше років (стаж віднімається також)",
+  "Дістань нову характеристику здоров'я з колоди для вибраного гравця",
+  "Дістань нову характеристику професії з колоди для вибраного гравця",
+  "Дістань нову характеристику хобі з колоди для вибраного гравця",
+  "Дістань нову характеристику факту з колоди для вибраного гравця",
+  "Наступного раунду примусь усіх гравців відкрити характеристику за твоїм бажанням",
+];
+const phobias = [
+  "Клаустрофобія (страх перед замкненим простором)",
+  "Агорафобія (страх перед відкритим простором)",
+  "Гемофобія (страх крові)",
+  "Арахнофобія (страх павуків)",
+  "Ніктофобія (страх темряви, ночі)",
+  "Талассофобія (страх морських подорожей, моря)",
+  "Урофобія (страх захотіти в туалет в умовах, коли зробити це неможливо)",
+  "Кінофобія (страх собак)",
+  "Пеладофобія (страх лисих людей)",
+  "Аквафобія (страх води)",
+  "Арахнофобія (страх павуків)",
+  "Авіафобія (страх польоту або літаків)",
+  "Гідрофобія (страх води)",
+  "Офідофобія (страх змій)",
+  "Гермофобія (страх бруду або мікробів)",
+  "Акрофобія (страх висоти)",
+  "Некрофобія (страх смерті або трупів)",
+  "Еметофобія (страх блювання)",
+  "Ентомофобія (страх комах)",
+  "Клауновідна фобія (страх клоунів)",
+  "Термофобія (страх тепла або жару)",
+  "Технофобія (страх технологій)",
+  "Колібрифобія (страх колібрі)",
+  "Немає фобії",
+];
+const stages = ["інкубаційний період", "20%", "50%", "75%", "100%"];
+const conditions = [""];
+
+// Функція для генерації випадкового елемента з масиву
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+// Функція для генерації картки гравця
+function generateCharacter() {
+  const profession = getRandomElement(professions);
+  const gender = getRandomElement(genders);
+  const age = Math.floor(Math.random() * 50) + 18; // Вік від 18 до 68 років
+  const lastDigitAge = age % 10;
+  let ageYears;
+
+  if (lastDigitAge === 1) {
+    ageYears = "рік";
+  } else if (lastDigitAge >= 2 && lastDigitAge <= 4) {
+    ageYears = "роки";
+  } else {
+    ageYears = "років";
+  }
+  const experience = Math.floor(Math.random() * (age - 18 + 1)); // Стаж від нуля до різниці віку та вісімнадцяти
+  const hobbyExperience = Math.floor(Math.random() * (age - 14 + 1)); // Стаж від нуля до різниці віку та чотирнадцяти
+  const health = getRandomElement(healths);
+  const lastDigitExperience = experience % 10;
+  let ageExperience;
+
+  if (lastDigitExperience === 1) {
+    ageExperience = "рік";
+  } else if (
+    (lastDigitExperience >= 2 && lastDigitExperience <= 4 && experience < 11) ||
+    (lastDigitExperience >= 2 && lastDigitExperience <= 4 && experience > 14)
+  ) {
+    ageExperience = "роки";
+  } else {
+    ageExperience = "років";
+  }
+  const phobia = getRandomElement(phobias);
+  const hobby = getRandomElement(hobbies);
+  const fact = getRandomElement(facts);
+  const baggage = getRandomElement(baggagelist);
+  const stage = getRandomElement(stages);
+  const condition = getRandomElement(conditions);
+
+  const actionCard1 = [getRandomElement(actioncards)];
+
+  let actionCard2;
+  do {
+    actionCard2 = getRandomElement(actioncards);
+  } while (actionCard2 == actionCard1);
+
+  const characterCard = `
+        <h2>Характеристики гравця</h2>
+        <div class="charwithbutton"><p class="professionpar" onclick="toggleDecoration(this)">&#128188; <strong>Професія та Стаж:</strong> ${profession}, ${experience} ${ageExperience}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#9892; <strong>Стать та Вік:</strong> ${gender}, ${age} ${ageYears}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#10084; <strong>Стан Здоров'я:</strong> ${health}, ${stage} (якщо можна застосувати)</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#128552; <strong>Фобія:</strong> ${phobia}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#9917; <strong>Хобі та Стаж:</strong> ${hobby}, ${hobbyExperience} р.</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#129523; <strong>Багаж:</strong> ${baggage}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#10071; <strong>Факт:</strong> ${fact}</p></div>
+        <p>&#9889; <strong>Картки дії:</strong></p>
+        <ul>
+            <li onclick="toggleDecoration(this)">${actionCard1}</li>
+            <li onclick="toggleDecoration(this)">${actionCard2}</li>
+        </ul>
+    `;
+
+  document.getElementById("characterCard").innerHTML = characterCard;
+  document.querySelector(".button-generate").disabled = true;
+}
+
+function toggleDecoration(paragraph) {
+  // Перевірка, чи вже є стиль line-through
+  if (paragraph.style.textDecorationLine === "line-through") {
+    // Якщо є, то видаляємо стиль
+    paragraph.style.textDecorationLine = "none";
+  } else {
+    // Інакше додаємо стиль
+    paragraph.style.textDecorationLine = "line-through";
+  }
+}
---
+++ bunkercard/scripten.js
@@ -0,0 +1,262 @@
+// Фіктивний список характеристик
+const professions = [
+  "Surgeon",
+  "Mechanical Engineer",
+  "PE Teacher",
+  "Architect",
+  "Cook",
+  "Pilot",
+  "Artist",
+  "Programmer",
+  "Nurse",
+  "Farmer",
+  "Psychologist",
+  "Sniper",
+  "Firefighter",
+  "Astronaut",
+  "Designer",
+  "Lawyer",
+  "Musician",
+  "Photographer",
+  "Waiter",
+  "Builder",
+  "Clown",
+  "Theater Actor",
+  "Instagram Blogger",
+  "Carpenter",
+  "Unemployed",
+  "Ecologist",
+  "Virologist",
+  "Diplomat",
+  "Police Officer",
+  "Paramedic",
+  "Electrician",
+  "Therapist",
+  "Lumberjack",
+  "Bouncer",
+  "Priest",
+];
+const healths = [
+  "Perfect Health",
+  "Pneumonia",
+  "Alcoholism",
+  "HIV",
+  "Chickenpox",
+  "Amnesia",
+  "Flu",
+  "Cold",
+  "Migraine",
+  "Dust Allergy",
+  "Missing Left Leg",
+  "Missing Two Fingers on Left Hand",
+  "Depression",
+  "Tourette Syndrome",
+  "Perfect Health",
+  "Asthma",
+  "Type 1 Diabetes",
+  "Hypertension",
+  "Gastritis",
+  "Dwarfism",
+  "Nail Fungus",
+  "Obesity",
+  "Drug Addiction",
+  "Gambling Addiction",
+  "Schizophrenia",
+  "Bipolar Disorder",
+  "Total Blindness",
+];
+const genders = ["Male", "Female"];
+const hobbies = [
+  "Traveling",
+  "Football",
+  "Cooking",
+  "Hiking",
+  "Cycling",
+  "Reading Books",
+  "Dancing",
+  "Fishing",
+  "Winemaking",
+  "Volunteering",
+  "Sewing",
+  "Swimming",
+  "Astronomy",
+  "Stalking",
+  "Weightlifting",
+  "Athletics",
+  "Photography",
+  "Boxing",
+  "Botany",
+  "Gardening",
+  "Knitting",
+  "Pottery",
+  "Drawing",
+  "Programming",
+  "Hobby Horsing",
+];
+const facts = [
+  "Robbed His Own Grandfather",
+  "Was in a Coma for 14 Years",
+  "Knows 5 Languages",
+  "Was Held Back a Year in School",
+  "Vegetarian",
+  "Has Two Higher Degrees",
+  "Plays Water Polo",
+  "Smokes Cigarettes",
+  "Has Four Children",
+  "Believes in a Global Conspiracy",
+  "Completed Military Training",
+  "Inherited 1 Million Dollars",
+  "Practices Fortune Telling",
+  "Stole a Yogurt from Store",
+  "Knowledgeable in Folk Medicine",
+  "Nazist",
+  "Acquainted with the President",
+  "Doesn't Wear Underwear",
+  "Completed First Aid Training",
+  "Partially Responsible for a Catastrophe",
+  "Survived in a road accident",
+  "Has Been Married Three Times",
+];
+const baggagelist = [
+  "Young Naturalist's Encyclopedia",
+  "Chihuahua",
+  "Compass",
+  "Scissors",
+  "Playing Cards",
+  "Hammer",
+  "Screwdriver",
+  "Multitool",
+  "Wheat Seed",
+  "Phone",
+  "Robot Vacuum Cleaner",
+  "Chess Board",
+  "Collection of 4500 Anecdotes",
+  "Unlimited Credit Card",
+  "Turkish Carpet",
+  "Toilet Paper",
+  "Knitting Kit",
+  "Umbrella",
+  "Crate of Strawberries",
+  "Light Bulb",
+  "Bible",
+  "Ficus",
+  "Box of 5.56mm Cartridges",
+  "Brick",
+  "Mouse Poison",
+];
+const actioncards = [
+  "In this round, you do not vote, but the votes against the chosen player are doubled",
+  "In this round, no one can vote against you",
+  "You can swap baggage with one person",
+  "You can swap health with one person",
+  "You can swap profession with one person",
+  "You can swap hobby with one person",
+  "You can swap a fact with one person",
+  "Rejuvenate the chosen player by 15 years, if they are 33 or older (experience is deducted as well)",
+  "Draw a new health characteristic from the deck for the chosen player",
+  "Draw a new profession characteristic from the deck for the chosen player",
+  "Draw a new hobby characteristic from the deck for the chosen player",
+  "Draw a new fact characteristic from the deck for the chosen player",
+  "In the next round, force all players to reveal a characteristic of your choosing",
+];
+const phobias = [
+  "Claustrophobia (fear of confined spaces)",
+  "Agoraphobia (fear of open spaces)",
+  "Hemophobia (fear of blood)",
+  "Arachnophobia (fear of spiders)",
+  "Nyctophobia (fear of darkness or night)",
+  "Thalassophobia (fear of sea travel or the sea)",
+  "Urophobia (fear of needing to use the toilet when it's impossible to do so)",
+  "Cynophobia (fear of dogs)",
+  "Peladophobia (fear of bald people)",
+  "Aquaphobia (fear of water)",
+  "Arachnophobia (fear of spiders)",
+  "Aviophobia (fear of flying or airplanes)",
+  "Hydrophobia (fear of water)",
+  "Ophidiophobia (fear of snakes)",
+  "Germophobia (fear of dirt or germs)",
+  "Acrophobia (fear of heights)",
+  "Necrophobia (fear of death or corpses)",
+  "Emetophobia (fear of vomiting)",
+  "Entomophobia (fear of insects)",
+  "Coulrophobia (fear of clowns)",
+  "Thermophobia (fear of heat or warmth)",
+  "Technophobia (fear of technology)",
+  "Colibriphobia (fear of hummingbirds)",
+  "No Phobia",
+];
+const stages = ["Incubation period", "20%", "50%", "75%", "100%"];
+const conditions = [""];
+
+// Функція для генерації випадкового елемента з масиву
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+// Функція для генерації картки гравця
+function generateCharacter() {
+  const profession = getRandomElement(professions);
+  const gender = getRandomElement(genders);
+  const age = Math.floor(Math.random() * 50) + 18; // Вік від 18 до 68 років
+  let ageYears;
+
+  if (age === 1) {
+    ageYears = "year";
+  } else {
+    ageYears = "years";
+  }
+  const experience = Math.floor(Math.random() * (age - 18 + 1)); // Стаж від нуля до різниці віку та вісімнадцяти
+  const hobbyExperience = Math.floor(Math.random() * (age - 14 + 1)); // Стаж від нуля до різниці віку та чотирнадцяти
+  const health = getRandomElement(healths);
+  let ageExperience;
+
+  if (experience === 1) {
+    ageExperience = "year";
+  } else {
+    ageExperience = "years";
+  }
+  const phobia = getRandomElement(phobias);
+  const hobby = getRandomElement(hobbies);
+  const fact = getRandomElement(facts);
+  const baggage = getRandomElement(baggagelist);
+  const stage = getRandomElement(stages);
+  const condition = getRandomElement(conditions);
+
+  const actionCard1 = [getRandomElement(actioncards)];
+
+  let actionCard2;
+  do {
+    actionCard2 = getRandomElement(actioncards);
+  } while (actionCard2 == actionCard1);
+
+  const characterCard = `
+        <h2>Player's characteristic</h2>
+        <div class="charwithbutton"><p class="professionpar" onclick="toggleDecoration(this)">&#128188; <strong>Job and Experience:</strong> ${profession}, ${experience} ${ageExperience}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#9892; <strong>Gender and Age:</strong> ${gender}, ${age} ${ageYears}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#10084; <strong>Health Condition:</strong> ${health}, ${stage} (if can be applied)</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#128552; <strong>Phobia:</strong> ${phobia}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#9917; <strong>Hobby and Experience:</strong> ${hobby}, ${hobbyExperience} y.</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#129523; <strong>Baggage:</strong> ${baggage}</p></div>
+        <div class="charwithbutton"><p onclick="toggleDecoration(this)">&#10071; <strong>Fact:</strong> ${fact}</p></div>
+        <p>&#9889; <strong>Action cards:</strong></p>
+        <ul>
+            <li onclick="toggleDecoration(this)">${actionCard1}</li>
+            <li onclick="toggleDecoration(this)">${actionCard2}</li>
+        </ul>
+    `;
+
+  document.getElementById("characterCard").innerHTML = characterCard;
+  document.querySelector(".button-generate").disabled = true;
+}
+
+function toggleDecoration(paragraph) {
+  // Перевірка, чи вже є стиль line-through
+  if (paragraph.style.textDecorationLine === "line-through") {
+    // Якщо є, то видаляємо стиль
+    paragraph.style.textDecorationLine = "none";
+  } else {
+    // Інакше додаємо стиль
+    paragraph.style.textDecorationLine = "line-through";
+  }
+}
